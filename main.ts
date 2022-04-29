a5066d284c6db659e02e34fc91ffff93b1bc18bd
Marciano00 <99951432+Marciano00@users.noreply.github.com>
2022-04-29 08:37:27 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showString("BB TROCHIN")
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    # # . # #
+    . . # . .
+    # . . . #
+    . # # # .
+    `)
+basic.forever(function () {
+	
+})
